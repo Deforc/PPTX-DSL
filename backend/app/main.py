dbe1f72ffceaea3ada698b754ea79c7a430734ba
Grebnev Gleb <aluminy-ch@yandex.ru>
2025-11-14 03:26:24 +0300

    fix backend: return cors origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,11 +23,9 @@ def create_app() -> FastAPI:
         redoc_url="/redoc",
     )
 
-    origins = [origin.strip() for origin in s.CORS_ORIGINS.split(",")]
-
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=s.CORS_ORIGINS,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
